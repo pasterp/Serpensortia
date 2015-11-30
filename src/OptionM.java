eb8d94bcca97b73f2fc9f831fd0e4cd2d98c8121
Lorchie <guillaume.kotnik@edu.univ-fcomte.fr>
2015-11-30 14:54:47 +0100

    Nouvelle class option

--- src/OptionM.java
+++ src/OptionM.java
@@ -1,6 +1,16 @@
+import javax.swing.*;
+
 /**
  * Created by lorchie on 30/11/15.
  */
 public class OptionM extends MenuVue {
+    public JComboBox<String> listeTaille;
 
+    public JButton up;
+    public JButton down;
+    public JButton left;
+    public JButton right;
+
+    public JButton retour;
+    }
 }
