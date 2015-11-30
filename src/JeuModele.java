c95403fafa10f6bc780f6797d697fc67328f4492
Pascal P <pascal.phelipot@gmail.com>
2015-11-30 14:07:37 +0100

    Maj Case -> Ajout texture

--- src/JeuModele.java
+++ src/JeuModele.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+
 /**
  * Created by pphelipo on 30/11/15.
  */
 public class JeuModele {
+    private final int taille = 50;
+    private Grille grille;
+    private ArrayList<Serpent> serpents;
+    private ArrayList<Fruit> fruits;
+
+    public JeuModele(){
+
+    }
+
+    public boolean isOver(){
+        return false;
+    }
+
 }
