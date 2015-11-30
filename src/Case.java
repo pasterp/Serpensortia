d8f01c1182dd2b40c69c8b7b6c345d7eef349d20
Pascal P <pascal.phelipot@gmail.com>
2015-11-30 13:22:22 +0100

    Case ok :)

--- src/Case.java
+++ src/Case.java
@@ -1,5 +1,22 @@
+import java.awt.*;
+
 /**
  * Created by pphelipo on 30/11/15.
  */
 public class Case {
+    private boolean passable;
+    private int score;
+    Image texture;
+
+    public int getScore(){
+        return score;
+    }
+
+    public boolean isPassable(){
+        return passable;
+    }
+
+    public Image getImage(){
+        return texture;
+    }
 }
