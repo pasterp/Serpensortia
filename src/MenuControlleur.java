38e2b83d694531fd031331a8da6947befe26e032
kenta77 <quentin.rimoux@live.fr>
2015-11-30 15:14:00 +0100

    En attente de Guillaume pour test (logiquement fonctionnel)

--- src/MenuControlleur.java
+++ src/MenuControlleur.java
@@ -1,10 +1,12 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 /**
  * Created by pphelipo on 30/11/15.
  */
-public class MenuControlleur{
+public class MenuControlleur implements KeyListener, ActionListener{
 
     MenuModele modele;
     MenuVue menuVue;
@@ -13,25 +15,123 @@ public class MenuControlleur{
     public MenuControlleur(MenuVue vue){
         modele = new MenuModele();
         menuVue = vue;
-        menuVue.setKeyListener(this);
     }
 
-    public void setKeys(){
+    public void setKeyUp() {
         KeyListener kl = new KeyListener() {
             @Override
-            public void keyTyped(KeyEvent e) {
-
-            }
+            public void keyTyped(KeyEvent e) {}
 
             @Override
             public void keyPressed(KeyEvent e) {
-
+                modele.setKeyUp(e.getKeyCode());
             }
 
             @Override
-            public void keyReleased(KeyEvent e) {
-
-            }
+            public void keyReleased(KeyEvent e) {}
         };
+        menuVue.setKeyListener(kl);
+    }
+
+    public void setKeyDown(){
+        KeyListener kl = new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {}
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                modele.setKeyDown(e.getKeyCode());
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {}
+        };
+        menuVue.setKeyListener(kl);
+    }
+
+    public void setKeyRight(){
+        KeyListener kl = new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {}
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                modele.setKeyRight(e.getKeyCode());
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {}
+        };
+        menuVue.setKeyListener(kl);
+    }
+
+    public void setKeyLeft(){
+        KeyListener kl = new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {}
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                modele.setKeyLeft(e.getKeyCode());
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {}
+        };
+        menuVue.setKeyListener(kl);
+    }
+
+    public void setKeyPause(){
+        KeyListener kl = new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {}
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                modele.setKeyPause(e.getKeyCode());
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {}
+        };
+        menuVue.setKeyListener(kl);
+    }
+
+    public void setGridSize(int size){
+        modele.setGridSize(size);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+//        if (e.getSource() == menuVue.up){
+//            System.out.println("Appuyez sur la touche pour HAUT");
+//            setKeyUp();
+//        }else if (e.getSource() == menuVue.down){
+//            System.out.println("Appuyez sur la touche pour BAS");
+//            setKeyDown();
+//        }else if (e.getSource() == menuVue.left){
+//            System.out.println("Appuyez sur la touche pour GAUCHE");
+//            setKeyLeft();
+//        }else if (e.getSource() == menuVue.right){
+//            System.out.println("Appuyez sur la touche pour DROITE");
+//            setKeyRight();
+//        }else if (e.getSource() == menuVue.listeTaille){
+//            setGridSize(Integer.parseInt(menuVue.listeTaille.getText()));
+//        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
