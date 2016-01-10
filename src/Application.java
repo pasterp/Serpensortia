637ac7dcdea3bf59c779314983f24074143de1a2
Pascal P <pascal.phelipot@gmail.com>
2016-01-10 21:36:17 +0100

    cleaned

--- src/Application.java
+++ src/Application.java
@@ -3,22 +3,7 @@ import javax.swing.*;
 /**
  * Created by pphelipo on 30/11/15.
  */
-/**
 
-public class Application {
-
-    public static void main(String[] argv) {
-        JeuModele jeuModele = new JeuModele(25);
-        JeuControlleur jc = new JeuControlleur(jeuModele);
-        JFrame frame = new JFrame("Jeu -- v0.3 beta");
-        frame.add(jc.getVue());
-        frame.pack();
-        frame.addKeyListener(jc);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-}
-     */
 public class Application extends JPanel {
 
  public static void main(String[] args) {
@@ -29,5 +14,5 @@ public class Application extends JPanel {
  ControlLabelMenu controlLabelMenu = new ControlLabelMenu(f, modelMenu,optionMenu);
  f.setControlLabelMenu(controlLabelMenu);
  }
- }
+}
 
